15da90c4bdaab79d6dd24b7aa32672b7f601ed85
lijsamuael <lijsamuael@gmail.com>
2023-02-02 11:06:36 -0800

    adding all the files at ones

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+sender = ARGV[0].scan(/(?<=\[from:)[^ ]+(?=\])/).join
+reciever = ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
+flags = ARGV[0].scan(/(?<=\[flags:)[^ ]+(?=\])/).join
+puts "#{sender},#{reciever},#{flags}"
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,2 @@
+Regular expressions
+---
